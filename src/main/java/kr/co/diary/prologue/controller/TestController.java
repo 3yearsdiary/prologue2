e44c80c37cc07edd70f4a26d9d518136306b3787
yoonsusususu <dbstngus91jp@gmail.com>
2020-02-23 20:44:58 +0900

    2020/02/23 ebalShampoo

--- src/main/java/kr/co/diary/prologue/controller/TestController.java
+++ src/main/java/kr/co/diary/prologue/controller/TestController.java
@@ -10,4 +10,9 @@ public class TestController {
     public String test() {
         return "Hello World!";
     }
+
+    @RequestMapping("/ebalShampoo")
+    public String ebalShampoo() {
+        return "샴푸는 두번하세요!";
+    }
 }
